260477b759cfca60b790d7ac25e11c316f173e28
vk-kushwaha <virendra.kushwaha0011@gmail.com>
2024-02-23 23:20:22 +0530

    changes

--- Project/apple calculator/app.js
+++ Project/apple calculator/app.js
@@ -3,6 +3,7 @@ let input = document.getElementById('input');
 let buttons = document.querySelectorAll('button');
 let string = "";
 
+
 let arr = Array.from(buttons);
 arr.forEach(buttons => {
     buttons.addEventListener('click', (event) => {
@@ -20,7 +21,43 @@ arr.forEach(buttons => {
             string = string.substring(0, string.length - 1);
             input.value = string;
         }
+        else if (event.target.innerHTML == '.') {
+            if (!input.value.includes('.')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
+        else if (event.target.innerHTML == '+') {
+            if (!input.value.includes('+')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
+        else if (event.target.innerHTML == '-') {
+            if (!input.value.includes('-')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
+        else if (event.target.innerHTML == '/') {
+            if (!input.value.includes('/')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
+        else if (event.target.innerHTML == '*') {
+            if (!input.value.includes('*')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
 
+        else if (event.target.innerHTML == '%') {
+            if (!input.value.includes('%')) {
+                string += event.target.innerHTML;
+                input.value = string;
+            }
+        }
         else {
             string += event.target.innerHTML;
             input.value = string;
